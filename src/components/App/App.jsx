148e675363fca7aa43c8700c9e5e365e91d63be2
VovaDev777 <polvovater@gmail.com>
2024-12-03 18:22:05 +0100

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import HomePage from "../../pages/HomePage/HomePage";
 import PortfolioPage from "../../pages/PortfolioPage/PortfolioPage";
+import SummaryPage from "../../pages/Summary/SummaryPage";
 
 function App() {
 
@@ -10,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}/>
         <Route path="/portfolio" element={<PortfolioPage />}/>
+        <Route path="/summary" element={<SummaryPage />}/>
+        <Route path="/register" element{}></Route>
+        <Route path="/login"></Route>
       </Routes>
     </Layout>
   )
--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+
 import css from './AppBar.module.css'
-import { Link } from 'react-router-dom'
-import HomePage from '../../pages/HomePage/HomePage'
+import { NavLink } from 'react-router-dom'
 
 const AppBar = () => {
   return (
-    <header className={css.headerCon}>
-        <Link to={<HomePage />}>
-            
-        </Link>
+    <header className={css.header}>
+        <div className={css.container}>
+            <div className={css.navigationCon}>
+                <p className={css.logo}><span className={css.firstLogoWord}>WEB</span>STUDIO</p>
+                <NavLink to='/' className={css.links}>Home</NavLink>
+                <NavLink to='/portfolio' className={css.links}>Portfolio</NavLink>
+                <NavLink to='/summary' className={css.links}>Summary</NavLink>
+            </div>
+            {/* <div className={css.registrationCon}> */}
+                <ul className={css.registrationList}>
+                    <NavLink className={css.links}>Register</NavLink>
+                    <NavLink className={css.links}>Login</NavLink>
+                </ul>
+            {/* </div> */}
+        </div>
     </header>
   )
 }
