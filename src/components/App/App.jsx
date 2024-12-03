a20db5eca822f31c35117a608198a853884405e5
VovaDev777 <polvovater@gmail.com>
2024-12-03 17:25:40 +0100

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import HomePage from "../../pages/HomePage/HomePage";
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+      </Routes>
+    </Layout>
   )
 }
 
