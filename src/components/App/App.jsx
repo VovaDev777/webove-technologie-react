a90071a43a02f44fd03e25ba2e248d5b4a6f0442
VovaDev777 <polvovater@gmail.com>
2024-12-03 18:27:45 +0100

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,8 @@ import Layout from "../Layout/Layout";
 import HomePage from "../../pages/HomePage/HomePage";
 import PortfolioPage from "../../pages/PortfolioPage/PortfolioPage";
 import SummaryPage from "../../pages/Summary/SummaryPage";
+import RegisterPage from "../../pages/RegisterPage/RegisterPage";
+import LoginPage from "../../pages/LoginPage/LoginPage";
 
 function App() {
 
@@ -12,8 +14,8 @@ function App() {
         <Route path="/" element={<HomePage />}/>
         <Route path="/portfolio" element={<PortfolioPage />}/>
         <Route path="/summary" element={<SummaryPage />}/>
-        <Route path="/register" element{}></Route>
-        <Route path="/login"></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
       </Routes>
     </Layout>
   )
--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -7,15 +7,15 @@ const AppBar = () => {
     <header className={css.header}>
         <div className={css.container}>
             <div className={css.navigationCon}>
-                <p className={css.logo}><span className={css.firstLogoWord}>WEB</span>STUDIO</p>
+                <NavLink to='/' className={css.logoLink}><p className={css.logo}><span className={css.firstLogoWord}>WEB</span>STUDIO</p></NavLink>
                 <NavLink to='/' className={css.links}>Home</NavLink>
                 <NavLink to='/portfolio' className={css.links}>Portfolio</NavLink>
                 <NavLink to='/summary' className={css.links}>Summary</NavLink>
             </div>
             {/* <div className={css.registrationCon}> */}
                 <ul className={css.registrationList}>
-                    <NavLink className={css.links}>Register</NavLink>
-                    <NavLink className={css.links}>Login</NavLink>
+                    <NavLink to='/register' className={css.links}>Register</NavLink>
+                    <NavLink to='/login' className={css.links}>Login</NavLink>
                 </ul>
             {/* </div> */}
         </div>
